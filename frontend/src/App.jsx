01e968219561f57c663022fda042178cb58c45f9
nosleepman1 <abbdallahdiouf.dev@gmail.com>
2025-12-19 05:04:33 +0000

    login features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from '@/lib/ThemeProvider'
 import Navbar from '@/components/layouts/Navbar'
+import { CardDemo } from './components/Card'
+
 
 function App() {
   
@@ -7,11 +9,13 @@ function App() {
   return (
     <ThemeProvider>
       <div className="min-h-screen bg-primaire dark:bg-fondSombre">
+        
         <Navbar />
         <main className="p-10">
           <h2 className="text-2xl font-bold text-gray-900 dark:text-gray-100">
             Bienvenue sur ImmoPrestige
           </h2>
+          < CardDemo />
           {/* Contenu principal */}
         </main>
       </div>
